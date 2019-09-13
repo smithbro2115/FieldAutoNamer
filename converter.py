cefa3c2d228cef96947d5d4d82d54380c5d701b6
Josh <smithbro2115@gmail.com>
2019-09-13 14:01:27 -0500

    - did alot of things

---
+++ converter.py
@@ -0,0 +1,127 @@
+import soundfile
+import time
+import numpy as np
+import os
+
+
+class AudioBuffer:
+	CHUNK_SIZE = 1024
+	VOLUME_PERCENTAGE = 100
+
+	def __init__(self):
+		self.sound_file = None
+		self.buffer = []
+		self.path = None
+		self.sound_info = None
+		self.finished = False
+		self.loaded = False
+		self.chunk_set = False
+		self.processes = []
+
+	def ready(self):
+		return len(self.buffer) > 0
+
+	# def buffer_loop(self):
+	# 	while True:
+	# 		while self.sound_file and len(self.buffer) < 50 and not self.finished:
+	# 			self.set_buffer()
+	# 			self.loaded = True
+	# 		time.sleep(.03)
+
+	def get_section(self, length, start):
+		if start < 0:
+			start = self.sound_info.duration + start
+		self.seek(start*1000)
+		chunk = self.sound_info.samplerate*length
+		return self.pad_sound(self.sound_file.read(chunk))
+
+	def get_sound_data(self):
+		chunk = self.sound_info.frames
+		return self.pad_sound(self.sound_file.read(chunk))
+
+	def write_for_speech(self, path):
+		self.stop()
+		self.load(path)
+		if self.sound_info.duration <= 20:
+			data = self.get_sound_data()
+		else:
+			first_ten = self.get_section(10, 0)
+			last_ten = self.get_section(10, -10)
+			data = first_ten+last_ten
+		return self.save_data(f"cache\\{os.path.basename(path)}", data)
+
+	def save_data(self, path, data):
+		soundfile.write(path, data, self.sound_info.samplerate)
+		return path
+
+	def load(self, path):
+		self.finished = False
+		self.path = path
+		self.sound_file = soundfile.SoundFile(path)
+		self.sound_info = soundfile.info(path)
+
+	# def set_buffer(self):
+	# 	data = self.get_correct_amount_of_channels(self.get_selected_channels(self.sound_file.read(self.CHUNK_SIZE)))
+	# 	padded_data = self.pad_sound(data)
+	# 	data_with_effects = padded_data, self.processes
+	# 	data_at_correct_volume = self.set_volume(data_with_effects)
+	# 	self.buffer.append(data_at_correct_volume)
+
+	def seek(self, goto):
+		"""
+		Set read from frame
+		:param goto:
+			should be in milliseconds
+		"""
+		goto_frame = int(self.sound_info.samplerate * (goto / 1000))
+		self.buffer = []
+		try:
+			self.sound_file.seek(goto_frame)
+		except RuntimeError:
+			self.sound_file.seek(self.sound_info.frames-1)
+
+	def stop(self):
+		self.sound_file = None
+		self.chunk_set = False
+		self.buffer = []
+
+	def pad_sound(self, data):
+		data_chunk_size = data.shape[0]
+		if 0 < data_chunk_size < self.CHUNK_SIZE:
+			data = np.pad(data, [(0, self.CHUNK_SIZE - data_chunk_size), (0, 0)], mode='constant')
+			self.finished = True
+		return data
+
+	# def get_buffer(self, outdata, frames, time, status):
+	# 	outdata[:] = self.buffer.pop(0)
+	# 	if self.finished and len(self.buffer) == 0:
+	# 		self.end_callback()
+
+	def sum_to_mono(self, data):
+		sound_data = np.ndarray(buffer=np.average(data, axis=1), shape=(data.shape[0], 1))
+		return sound_data
+
+	# def get_selected_channels(self, data):
+	# 	if len(self.PLAY_INDIVIDUAL_CHANNELS) > 0:
+	# 		return self._get_selected_channels_from_play_channels(data)
+	# 	return data
+	#
+	# def _get_selected_channels_from_play_channels(self, data):
+	# 	channels = self.get_individual_channels(data)
+	# 	selected_channels = np.ndarray((data.shape[0], 0))
+	# 	for channel_number in self.PLAY_INDIVIDUAL_CHANNELS:
+	# 		try:
+	# 			selected_channels = np.concatenate((selected_channels, channels[channel_number-1]), axis=1)
+	# 		except IndexError:
+	# 			continue
+	# 	return selected_channels
+
+	@staticmethod
+	def get_individual_channels(data):
+		sound_data = np.hsplit(data, data.shape[1])
+		return sound_data
+
+
+# audio_buffer = AudioBuffer(print)
+# audio_buffer.load("Z:\\SFX Library\\bin\\MONO\\TEST\\ZOOM0023_3.WAV")
+# audio_buffer.get_ten_seconds(-10)
--- main.py
+++ main.py
@@ -1,27 +1,146 @@
 import speech_recognition as sr
+import os
 import pydub
+import converter
 
-AUDIO_FILE = "C:\\Users\\smith\\Downloads\\ZOOM0007_3.WAV"
+AUDIO_FILE = "Z:\\SFX Library\\bin\\Combined\\ZOOM0027_1_2_3.WAV"
 AUDIO_FILE_1 = "C:\\Users\\smith\\Downloads\\Sports Human Voice Male Young Man Basketball" \
              " Announcer It's Fifty Six To Forty Five At The Half 02.wav"
 # pydub.AudioSegment.ffmpeg = "D:\\Programming\\FieldAutoNamer\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
-audio_editor = pydub.AudioSegment.from_wav(AUDIO_FILE)
-first_fifteen = audio_editor[:8000]
-last_fifteen = audio_editor[15000:]
-first_fifteen = first_fifteen.set_frame_rate(44100)
-print(first_fifteen.frame_rate)
-audio_data = first_fifteen.export("cache\\Zoom.wav", format='wav', parameters=["-ar", "441000"])
-# use the audio file as the audio source
-r = sr.Recognizer()
-with sr.AudioFile("cache\\Zoom.wav") as source:
-    audio = r.record(source)  # read the entire audio file
 
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print(r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+def get_renamed_files_dict(path, reference_number, filter_words, check_words):
+    reference_file_paths = get_file_names(path, reference_number)
+    for key, value in reference_file_paths.items():
+        file_type = os.path.splitext(value)[1]
+        audio_path = get_audio_segment(value)
+        raw_text = recognize_audio(audio_path)
+        if raw_text:
+            processed = get_file_name_from_google_words(raw_text, filter_words, check_words)
+            if processed:
+                print(f'Found {value} as {processed}')
+                reference_file_paths[key] = f"{processed}{file_type}"
+        else:
+            reference_file_paths[key] = f"{key}{file_type}"
+    return reference_file_paths
+
+
+def clear_cache():
+    files = [os.path.join('cache', f) for f in os.listdir('cache') if os.path.isfile(os.path.join('cache', f))]
+    for file in files:
+        os.remove(file)
+
+
+def get_file_names(path, reference_file_number):
+    paths = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    return get_all_reference_paths(paths, reference_file_number)
+
+
+def get_all_reference_paths(paths, reference_file_number):
+    reference_paths = {}
+    for path in paths:
+        if path.endswith(f"{reference_file_number}.WAV"):
+            basename = os.path.basename(path)
+            reference_paths[basename[:basename.index('_')]] = path
+    return reference_paths
+
+
+def rename_file(old_path, new_path):
+    os.rename(old_path, new_path)
+
+
+def get_first_and_last(path):
+    audio = converter.AudioBuffer()
+    return audio.write_for_speech(path)
+
+
+def get_audio_segment(path):
+    path = get_first_and_last(path)
+    audio_segment = pydub.AudioSegment.from_wav(path)
+    audio_segment = audio_segment.set_frame_rate(44100)
+    audio_path = f"cache\\{os.path.basename(path)}"
+    audio_segment.export(audio_path, format='wav')
+    return audio_path
+
+
+def get_correct_audio_segment_duration(audio_segment):
+    if audio_segment.duration_seconds > 20:
+        first_five = audio_segment[:10000]
+        last_five = audio_segment[-10000:]
+        audio_segment = first_five+last_five
+    return audio_segment
+
+
+def recognize_audio(path):
+    r = sr.Recognizer()
+    with sr.AudioFile(path) as source:
+        audio = r.record(source)
+    try:
+        return r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+def get_file_name_from_google_words(string, filter_words, check_words):
+    filename = ''
+    words = string.split()
+    if check_words:
+        filename = get_string_that_is_checked(words, filter_words)
+    elif filter_words:
+        for index, word in enumerate(words):
+            filename = f"{filename} {filter_word(word, index)}"
+    else:
+        filename = string
+    try:
+        return filename.strip().title()
+    except AttributeError:
+        return filename
+
+
+def get_string_that_is_checked(words, filter_words):
+    filename = ''
+    input_began = False
+    last_word = ''
+    for index, word in enumerate(words):
+        if input_began:
+            if check_if_ended(word):
+                input_began = False
+            else:
+                processed_word = filter_word(word, index) if filter_words else word
+                filename = f"{filename} {processed_word}"
+        else:
+            input_began = check_if_started(word, last_word)
+        last_word = word
+    if filename != "":
+        return filename
+
+
+def check_if_started(word, last_word):
+    if last_word == 'this' and word == 'is' or word == 'append':
+        return True
+    return False
+
+
+def check_if_ended(word):
+    end_list = ['end', 'stop']
+    if word in end_list:
+        return True
+    return False
+
+
+def filter_word(word, index):
+    filter_list = ['um', 'uh', 'em']
+    filter_leading_list = ['this', 'is']
+    filter_leading_index_list = [0, 1]
+    if word in filter_list:
+        return ''
+    if word in filter_leading_list and index in filter_leading_index_list:
+        return ''
+    return word
+
+
+print(get_renamed_files_dict('Z:\\SFX Library\\bin\\MONO\\TEST', 3, True, False))
+clear_cache()
+
